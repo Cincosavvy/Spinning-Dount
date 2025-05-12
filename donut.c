d41cb2d6068d162be3b1a273ab1413afbcbd5c1b
Marnier Crockton <36340259+Cincosavvy@users.noreply.github.com>
2025-05-12 13:34:28 -0400

    Update donut.c

--- donut.c
+++ donut.c
@@ -1,27 +1,29 @@
-//Create main
-int main() {
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
 
-    //Create floats
+int main() {
+    // Create floats
     float A = 0, B = 0;
     float i, j;
     int k;
     float z[1760];
     char b[1760];
 
-    //prints out the random characters
+    // Prints out the random characters
     printf("\x1b[2J");
-    
-    //start for 
+
+    // Start infinite loop
     for (;;) {
-        //creates the memset for b and z
+        // Create memset for b and z
         memset(b, 32, 1760);
         memset(z, 0, 7040);
 
-        //start for loop
-        for( j = 0; j < 6.28; j += 0.07) {
-            for( i = 0; i < 6.28; i += 0.02) {
-
-                //creates floats in the loop
+        // Start for loop for animation
+        for (j = 0; j < 6.28; j += 0.07) {
+            for (i = 0; i < 6.28; i += 0.02) {
+                // Create floats in the loop
                 float c = sin(i);
                 float d = cos(j);
                 float e = sin(A);
@@ -34,34 +36,35 @@ int main() {
                 float n = sin(B);
                 float t = c * h * g - f * e;
 
-                //ints
+                // ints for pixel positions
                 int x = 40 + 30 * D * (1 * h * m - t * n);
                 int y = 12 + 15 * D * (1 * h * n + t * m);
                 int o = x + 80 * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
 
-                //start if statement
-                if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
-
-                    //store D into z of o
+                // Store values in array
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;
-                    b[o] = ".,-~##@$*)"[N > 0 ? N : 0];
+                    b[o] = ",.-~#@$*)"[N > 0 ? N : 0]; // Proper assignment
                 }
             }
         }
-        //print it out
+
+        // Print it out
         printf("\x1b[H");
-        
-        //start for to manage speed of donut 
-        for(k = 0; k < 1761; k++) {
-            putchar(k % 80 ? b[k] : 10);
-            //A + B are equal to ?
-            A += 0.00004;
-            B += 0.00002;
+
+        // Loop to manage speed of donut 
+        for (k = 0; k < 1761; k++) {
+            putchar(k % 80 ? b[k] : 10); // Print the characters
         }
-        //timer
+
+        // Adjust rotation speeds
+        A += 0.00004;
+        B += 0.00002;
+
+        // Sleep to control speed of animation
         usleep(30000);
     }
-    //return
+
     return 0;
 }
